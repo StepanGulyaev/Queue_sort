bd9d5d7dd5ab127af0f36acd55dd997360ef6fe0
StepanGulyaev <91908161+StepanGulyaev@users.noreply.github.com>
2021-11-23 03:05:14 +0300

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -11,7 +11,6 @@ class Queue
         int is_not_empty(); // check if queue is empty
         int check_pos_exist(unsigned int pos); // check position on existence 
 
-
     public:
         size_t size;
         unsigned long long int N_op;
@@ -22,15 +21,15 @@ class Queue
 
         void push(const int el); // add element
         void push(const std::vector<int> newValues); // add few elements
-        int pop(); // delete first element and return it's value
         int peek(); // return value of the first element 
+        int pop(); // delete first element and return it's value
         void clear(); // delete all elements from array
         void clear(size_t pos); // delete all elements before the certain position ()
         void clear_N_op(); //clear operation counter 
 
-        void set(const int newValue,size_t pos); // set new value to element on n-position
         int get(size_t pos); // get value from element on n-position
-
+        void set(const int newValue,size_t pos); // set new value to element on n-position
+        
         int& operator[] (unsigned int index); // overload the "[]"
 
         void sort(); //insertion sort of queue
